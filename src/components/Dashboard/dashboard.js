dc21f6b6d72e42ca3d75a46bb2b74418c961bc82
Zee Celest <zeecelest@gmail.com>
2019-12-31 11:47:51 -0500

    updating dashboard.js

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -2,3 +2,14 @@
 //create a start practicing button
 //show list of words to practice
 //with each word should be a count of how many correct and how many wrong
+import React from 'react';
+
+export default class Dashboard extends React.Component {
+    render() {
+        return (
+            
+        )
+    }
+
+}
+
